ee78f5504082e53cd53d8713510a9d99a2489971
Mohamed <MohamedEalbd>
2020-06-30 17:22:24 +0200

    reminder

--- lib/locator.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:reminder/push_nofication.dart';
-
-GetIt locator = GetIt.instance;
-void setupLocator(){
-  locator.registerLazySingleton(() => PushNotificationService());
-}
